874fba159817e3a069dd6a27fc0cff7bf000c052
AmauryD <deflorenne.amaury@gmail.com>
2023-12-22 12:06:35 +0100

    ignore node_modules

---
+++ project-2/README.md
@@ -0,0 +1,9 @@
+# PNPM
+
+## Patcher une dépendance
+
+Permet d'overrider les fichiers d'un package si quelque chose se passe mal. à utiliser avec parcimonie.
+
+```
+    pnpm patch <lepackage>
+```
--- project-2/index.js
+++ project-2/index.js
@@ -19,13 +19,23 @@ const {
 }
 */
 
-
-
 async function transformImageDramix() {
     // on charge l'image
     let image = await Image.load('otter_banana.png');
     // on dilate Dramix
-    let dilated = image.dilate();
+    let dilated = image
+        .grey()
+        .dilate({
+            kernel: [[1, 0.75, 1], [1, 1, 1], [1, 0.5, 1]],
+            iterations: 2
+        });
+
     // on sauvegarde Dramix dilaté
-    await dilated.save('dramix-dilate');
+    await dilated.save('dramix-dilate.png');
 }
+
+transformImageDramix()
+    // une fois que la fonction est exécutée, on affiche un succès
+    .then(() => console.log('Dilation de Dramix avec succès'))
+    // si il y'a une erreur, on affiche une erreur
+    .catch((error) => console.log('Dramix n\'était pas prêt', error))
