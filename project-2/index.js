50b37498dfda2d925f9394a8ed6ce84fcd18288f
AmauryD <deflorenne.amaury@gmail.com>
2023-12-22 11:45:15 +0100

    dilatons Dramix

--- project-2/index.js
+++ project-2/index.js
@@ -1,5 +1,31 @@
-import 
 
-export function coucou() {
-    
+// import => ECMASCRIPT
+import { Image } from 'image-js'; // importe que Image
+// vous pouvez voir ça comme une déstructuration d'objet. 
+
+/** 
+// require => commonJS
+const { Image } = require('image-js'); // importe tout le module 
+
+export {
+    Image: "a"
+}
+
+const {
+    Image
+} = {
+    Image: {},
+    Truc: 0
+}
+*/
+
+
+
+async function transformImageDramix() {
+    // on charge l'image
+    let image = await Image.load('otter_banana.png');
+    // on dilate Dramix
+    let dilated = image.dilate();
+    // on sauvegarde Dramix dilaté
+    await dilated.save('dramix-dilate');
 }
