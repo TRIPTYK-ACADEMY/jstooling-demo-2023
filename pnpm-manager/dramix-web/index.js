a9769b48e6cd179b6fbe6694b24d2a2cf22dda0a
AmauryD <deflorenne.amaury@gmail.com>
2023-12-22 14:06:12 +0100

    exercice mini

--- pnpm-manager/dramix-web/index.js
+++ pnpm-manager/dramix-web/index.js
@@ -2,11 +2,14 @@ import { transformImageDramix } from 'dramix-transformer';
 
 // Faire un script qui lorsque l'on lui passe un argument <couleur>, génère l'image de Dramix avec la couleur spécifiée en argument
 
-// node index.js red
-// => génère une photo de dramix rouge
-// node index.js green
-// => génère une photo de dramix verte
+// node index.js 50
+// => génère une photo de dramix tournée à 50 degrés
+// node index.js 70
+// => génère une photo de dramix tournée à 70 degrés
+// si le nombre est négatif, déclenche une erreur
 
+// process.argv => arguments passés au programme
+console.log(process.argv);
 
 transformImageDramix(50)
     // une fois que la fonction est exécutée, on affiche un succès
